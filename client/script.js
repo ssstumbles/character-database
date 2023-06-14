1f2d82971743c896f4b52fb622c16ba1bbe6ace2
ssstumbles <sara.rosic@gmail.com>
2023-06-14 16:49:12 -0400

    starting axios calls

--- client/script.js
+++ client/script.js
@@ -1,1 +1,20 @@
 let url = `https://localhost:3001`
+
+
+const getSpecies = async () => {
+const pullSpecies = await axios.get('http://localhost:3001/API/species_seed')
+    console.log(pullSpecies)
+}
+getSpecies()
+
+getBranches = async () => {
+    const pullBranches = await axios.get('http://localhost:3001/API/branches')
+    console.log(pullBranches)
+}
+getBranches()
+
+getCharacters = async () => {
+    const pullCharacters = await axios.get('http://localhost:3001/API/characters')
+    console.log(pullCharacters)
+}
+getCharacters()
