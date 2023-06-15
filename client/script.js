3483380f581d089984a1526caaec926962908b2a
ssstumbles <sara.rosic@gmail.com>
2023-06-15 10:06:54 -0400

    axios and DOM manipulation started

--- client/script.js
+++ client/script.js
@@ -1,11 +1,28 @@
 let url = `https://localhost:3001`
 
+const elfSelect = document.querySelector('#elf')
+const orcSelect = document.querySelector('orc')
+const spriteSelect = document.querySelector('sprite')
+const merSelect  = document.querySelector('mer')
 
 const getSpecies = async () => {
 const pullSpecies = await axios.get('http://localhost:3001/API/species_seed')
     console.log(pullSpecies)
 }
 getSpecies()
+
+const lavaElf = document.querySelector('#lava-elf')
+const mountainElf = document.querySelector('#mountain-elf')
+const frostOrc = document.querySelector('#frost-orc')
+const duneOrc = document.querySelector('#dune-orc')
+const mushroomSprite = document.querySelector("#mushroom.sprite")
+const twigSprite = document.querySelector('#twig-sprite')
+const grassSprite = document.querySelector('#grass-sprite')
+const oceanMer = document.querySelector('#ocean-mer')
+const lagoonMer = document.querySelector('#lagoon-mer')
+const riverMer = document.querySelector('#river-mer')
+const dockGoblin = document.querySelector('#dock-goblin')
+const swampGoblin = document.querySelector('#swamp-goblin')
 
 getBranches = async () => {
     const pullBranches = await axios.get('http://localhost:3001/API/branches')
